7ecbe8d6a54c00c9d06139969e2929ca2d7d304a
ganeshkamble-411 <gk6569471@gmail.com>
2022-04-27 17:46:35 +0530

    new addition

---
+++ src/Nav.js
@@ -0,0 +1,25 @@
+import React, { useEffect } from 'react';
+import "./Nav.css";
+
+function Nav() {
+
+    useEffect(())
+
+  return (
+    <div>
+        {/* Nav */}
+        <img
+        className='nav__logo'
+        src = "https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+        alt="Netflix Logo"
+        />
+        <img
+        className='nav__avatar'
+        src="https://www.google.com/search?q=.svg+images+of+profile&sxsrf=ALiCzsa-Z1R3OXzrhHwcYL_DfHYNZffQ4Q:1651057673864&tbm=isch&source=iu&ictx=1&vet=1&fir=WstIZq3IgRE_dM%252C3vG4DIAGZjyBxM%252C_%253B5F52f7hk_anW7M%252Cmx8tD-67TU4U5M%252C_%253BBcWdt6jvD1g-xM%252CUagNtViPVDBRoM%252C_%253BECAzCBsLQOV5xM%252C3vG4DIAGZjyBxM%252C_%253BlGfVGQJwvKX-qM%252CQcDS8eoDdcwN_M%252C_%253B7l1uGCQ-gnEnCM%252C-1DxMnFZSekz0M%252C_%253BKGmmZKqVH-I6eM%252Cvce_7rMT4eJR2M%252C_%253BuSE2HuJ4-ueWoM%252CV0DtK1O-TxOnxM%252C_%253BM23rYZm0cm4y1M%252CmCcvTNoZKTxFTM%252C_%253B1eKeJ7fvU3uuUM%252CBKQMzvdSaWuXUM%252C_%253BTGiBVNlGEEcFPM%252C1GTwGpxB9pFPrM%252C_%253BJc7T3lEI3W97dM%252C3vG4DIAGZjyBxM%252C_%253BsCsU1ld9cNqCWM%252C5F5nqWrQ5esNzM%252C_%253BQGQteQy1-3ib0M%252C7q8F8Xi5EXeDkM%252C_%253B_bxTSGdjAc7JWM%252Cvce_7rMT4eJR2M%252C_&usg=AI4_-kRmPgwwh4-IoP15pH6wKsRvHX2ymA&sa=X&ved=2ahUKEwiN1Z2RjbT3AhUYS2wGHdkxCzcQ9QF6BAgGEAE#imgrc=WstIZq3IgRE_dM"
+        alt="Netflix Logo"
+        />
+    </div>
+  )
+}
+
+export default Nav
